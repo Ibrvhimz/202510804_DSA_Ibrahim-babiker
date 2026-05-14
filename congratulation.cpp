ad30b4c0f6464fa60c966da1d3fa84bc7d89ab75
Ibrvhimz <hemonx999@gmail.com>
2026-05-14 22:57:24 +0200

    Create congratulation.cpp

---
+++ congratulation.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  string name = "a";
+
+  cout<<" enter your name : ";
+  cin>>name;
+  cout<<"congratulations "<<name<<endl;
+
+
+
+
+    return 0;
+}
